353e75142a2cfa88d537479e45fef5f1ae0856a8
Benoit Gschwind <gschwind@gnu-log.net>
2015-11-10 21:30:04 +0100

    import list_oom_adj.py output

--- list_oom_adj.py
+++ list_oom_adj.py
@@ -11,19 +11,39 @@ number = re.compile("[+-]?\d+")
 l = []
 
 for d in os.listdir("/proc"):
- if number.match(d):
-  f = open(os.path.join("/proc", d, "oom_score_adj"), "r")
-  adj = int(f.read())
-  f.close()
-  f = open(os.path.join("/proc", d, "stat"), "r")
-  cmd = f.read().split(' ')[1]
-  f.close()
-  st = os.stat(os.path.join("/proc", d))
-  us = getpwuid(st.st_uid)
-  gr = getgrgid(st.st_gid)
-  if len(cmd) == 0:
-   cmd = "unknown#%s"%(d,)
-  l.append((cmd, us.pw_name, gr.gr_name, adj))
+ try:
+  if number.match(d):
+   f = open(os.path.join("/proc", d, "oom_score_adj"), "r")
+   adj = int(f.read())
+   f.close()
+   f = open(os.path.join("/proc", d, "stat"), "r")
+   cmd = f.read().split(' ')[1]
+   f.close()
+   st = os.stat(os.path.join("/proc", d))
+   us = getpwuid(st.st_uid)
+   gr = getgrgid(st.st_gid)
+   if len(cmd) == 0:
+    cmd = "unknown#%s"%(d,)
+   l.append((cmd, us.pw_name, gr.gr_name, adj))
+ except FileNotFoundError:
+  pass
+
+ll = []
+if len(l) > 0:
+ prev = l[0]
+ count = 1
+ for v in l[1:]:
+  if v != prev:
+   if count > 1:
+    ll.append(("%s*%d"%(prev[0], count), prev[1], prev[2], prev[3], count))
+   else:
+    ll.append((prev[0], prev[1], prev[2], prev[3], count))
+   prev = v
+   count = 1
+  else:
+   count += 1
+
+l = ll
 
 max_cmd = 0
 max_usr = 0
@@ -37,6 +57,7 @@ for v in l:
   max_grp = len(v[2])
 
 for v in l:
- fmt = "{{0:<{0}}}  {{1:<{1}}}  {{2:<{2}}}  {{3:>8d}}".format(max_cmd, max_usr, max_grp).format(v[0], v[1], v[2], v[3])
- print(fmt)
+ fmt = "{{0:<{0}}}  {{1:<{1}}}  {{2:<{2}}}  {{3:>8d}}".format(max_cmd, max_usr, max_grp)
+ out = fmt.format(v[0], v[1], v[2], v[3])
+ print(out)
 
